f20493daab728626c1843537e853b9e2d9f53e58
spiner321 <luckyboy3214@naver.com>
2023-01-29 23:23:35 +0900

    easy runch sh

--- run4_lambdawebui.sh
+++ run4_lambdawebui.sh
@@ -2,4 +2,4 @@
 python modules/duplicate.py -d1 ./generated -d2 ../stable-diffusion-webui/generated
 python modules/duplicate.py -d1 ./logs -d2 ../stable-diffusion-webui/logs
 python modules/duplicate.py -c -d1 ./modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf 
-pm2 start webui.py --name="webui" -f --interpreter python -- --listen --cors-allow-origins "*" --api --ui-debug-mode --enable-insecure-extension-access
+pm2 start webui.py --name="webui" -f --interpreter python -- --listen --cors-allow-origins "*" --api --enable-insecure-extension-access
