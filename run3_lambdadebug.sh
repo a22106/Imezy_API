bbf5ea75ff2e779b85c0b4c1fe19d0e18612dab8
spiner321 <luckyboy3214@naver.com>
2023-02-01 19:51:05 +0900

    update sh

--- run3_lambdadebug.sh
+++ run3_lambdadebug.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/generated -d2 ../stable-diffusion-webui/generated
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/logs -d2 ../stable-diffusion-webui/logs
+python modules/duplicate.py -c -d1 ../stable-diffusion-webui-test/modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf 
 python webui.py --listen --cors-allow-origins "*" --api --ui-debug-mode
