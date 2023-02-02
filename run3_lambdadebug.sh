ae2d7de37e2e0b0ab387dfe7acef3ffb46fdd3f4
a22106 <bk22106@gmail.com>
2023-02-02 14:41:10 +0900

    update packages requirements

--- run3_lambdadebug.sh
+++ run3_lambdadebug.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-python modules/duplicate.py -d1 ../stable-diffusion-webui-test/generated -d2 ../stable-diffusion-webui/generated
-python modules/duplicate.py -d1 ../stable-diffusion-webui-test/logs -d2 ../stable-diffusion-webui/logs
-python modules/duplicate.py -c -d1 ../stable-diffusion-webui-test/modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf 
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/generated -d2 ../stable-diffusion-webui/generated # 생성 이미지 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/logs -d2 ../stable-diffusion-webui/logs # 로그 복사
+python modules/duplicate.py -c -d1 ../stable-diffusion-webui-test/modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf # 설정 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/models -d2 ../stable-diffusion-webui/models # 모델 복사
 python webui.py --listen --cors-allow-origins "*" --api --ui-debug-mode
