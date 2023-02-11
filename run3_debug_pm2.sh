33fe5c4728f726c6d0e8cb9078237dbb29770131
a22106 <bk22106@gmail.com>
2023-02-11 10:21:20 +0900

    fix shell script

--- run3_debug_pm2.sh
+++ run3_debug_pm2.sh
@@ -3,4 +3,6 @@ python modules/duplicate.py -d1 ../stable-diffusion-webui-test/generated -d2 ../
 python modules/duplicate.py -d1 ../stable-diffusion-webui-test/logs -d2 ../stable-diffusion-webui/logs # 로그 복사
 python modules/duplicate.py -c -d1 ../stable-diffusion-webui-test/modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf # 설정 복사
 python modules/duplicate.py -d1 ../stable-diffusion-webui-test/models -d2 ../stable-diffusion-webui/models # 모델 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/test -d2 ../stable-diffusion-webui/test # 테스트 파일 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/embeddings -d2 ../stable-diffusion-webui/embeddings # 임베딩 복사
 pm2 start webui.py --name="webui" -f --interpreter python -- --listen --cors-allow-origins "*" --api --ui-debug-mode
--- run4_webui_pm2.sh
+++ run4_webui_pm2.sh
@@ -3,6 +3,8 @@ python modules/duplicate.py -d1 ../stable-diffusion-webui-test/generated -d2 ../
 python modules/duplicate.py -d1 ../stable-diffusion-webui-test/logs -d2 ../stable-diffusion-webui/logs # 로그 복사
 python modules/duplicate.py -c -d1 ../stable-diffusion-webui-test/modules/api/conf -d2 ../stable-diffusion-webui/modules/api/conf # 설정 복사
 python modules/duplicate.py -d1 ../stable-diffusion-webui-test/models -d2 ../stable-diffusion-webui/models # 모델 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/test -d2 ../stable-diffusion-webui/test # 테스트 파일 복사
+python modules/duplicate.py -d1 ../stable-diffusion-webui-test/embeddings -d2 ../stable-diffusion-webui/embeddings # 임베딩 복사
 
 # Get the device-id from the input or set a default value
 device_id=${1:-3}
